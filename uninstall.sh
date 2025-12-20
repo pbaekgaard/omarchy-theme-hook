da07484597ffac446331c983e903ab043e2e6fac
imbypass <coleschaefer1@gmail.com>
2025-12-20 09:27:47 -0500

    updated uninstall script; should attempt to remove some theming now.

--- uninstall.sh
+++ uninstall.sh
@@ -11,6 +11,15 @@ rm -rf $HOME/.local/share/omarchy/bin/theme-hook-update
 rm -rf $HOME/.config/omarchy/hooks/theme-set.d/
 rm -rf $HOME/.config/omarchy/hooks/theme-set
 
+echo "Attempting to unapply theme hook.."
+if command -v python >/dev/null 2>&1; then
+    cd $HOME/.local/share/steam-adwaita && ./install.py --uninstall > /dev/null 2>&1
+fi
+if command -v spicetify >/dev/null 2>&1; then
+    spicetify restore
+fi
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita
+
 echo "Uninstalled theme hook!"
 
 omarchy-show-done
